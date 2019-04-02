b911f1b0646fb6b8a3e37a3e89c3997405561bcf
slivermasterz <lokmanslivermaster@gmail.com>
2019-04-01 21:34:57 -0700

    WIP Parallel Generator

--- src/ParticleGenerator.cpp
+++ src/ParticleGenerator.cpp
@@ -1,21 +1,65 @@
 #include "ParticleGenerator.h"
 
-void ParticleGenerator::generateParticle(vector<Particle> particles) {
+void ParticleGenerator::generateParticle(vector<Particle> * particles) {
 	static thread_local std::mt19937 generator;
 	Particle * p = new Particle();
 	p->position.set(randW(), randH(), 0);
-	std::uniform_real_distribution<float> distrubution(0.0, 1.0);
+	std::uniform_real_distribution<float> distrubution(-1.0, 1.0);
 	ofVec3f dir = ofVec3f( distrubution(generator), distrubution(generator), 0);
 	p->velocity.set(dir * 10);
 	p->color.set(randColor());
-	particles.push_back(*p);
+	particles->push_back(*p);
 }
 
-void ParticleGenerator::generateParticle(vector<Particle> particles, int n) {
+void ParticleGenerator::generateParticle(vector<Particle> * particles, int n) {
 	for (int i = 0; i < n; i++)
 	{
 		generateParticle(particles);
 	}
+}
+
+void ParticleGenerator::generateParticlePara(vector<Particle> * particles, int n) {
+	int nThreads = omp_get_num_threads();
+	vector<vector<Particle> *> vectors;
+	for (int i = 0; i < nThreads; i++) {
+		vectors.push_back(new vector<Particle>);
+	}
+
+#pragma omp parallel
+	{
+		int id, i, istart, iend;
+		id = omp_get_thread_num();
+		istart = id * n / nThreads;
+		iend = (id + 1) * n / nThreads;
+		if (id == nThreads - 1) iend = n;
+		for (i = istart; i < iend; i++) {
+			generateParticle(vectors[id]);
+		}
+	}
+	
+	/*
+	divide(vectors, 0, vectors.size());
+	free(particles);
+	particles = vectors[0];
+	*/
+}
+
+void ParticleGenerator::merge(vector<Particle> * A, vector<Particle> * B) {
+	vector<Particle> * AB = new vector<Particle>;
+	AB->reserve(A->size() + B->size());
+	AB->insert(AB->end(), A->begin(), A->end());
+	AB->insert(AB->end(), B->begin(), B->end());
+	free(A);
+	free(B);
+	A = AB;
+}
+
+void ParticleGenerator::divide(vector<vector<Particle> *> vectors, int left, int right) {
+	if (left >= right) return;
+	int mid = left + (right - left) / 2;
+	divide(vectors, left, mid);
+	divide(vectors, mid + 1, right);
+	merge(vectors[left], vectors[right]);
 }
 
 int ParticleGenerator::intRand(const int & min, const int & max) {
--- src/ParticleGenerator.h
+++ src/ParticleGenerator.h
@@ -1,14 +1,21 @@
 #pragma once
 #include <random>
 #include "Particle.h"
+#include "omp.h"
 
 class ParticleGenerator {
 	public:
-		void generateParticle(vector<Particle>);
-		void generateParticle(vector<Particle>,int);
+		void generateParticle(vector<Particle> *);
+		void generateParticle(vector<Particle> *,int);
+
+		void generateParticlePara(vector<Particle> *, int);
+		void merge(vector<Particle> *, vector<Particle> *);
+		void divide(vector<vector<Particle> *>, int, int);
 	
 		int intRand(const int & min, const int & max);
 		int randW();
 		int randH();
 		ofColor randColor();
+
+
 };
