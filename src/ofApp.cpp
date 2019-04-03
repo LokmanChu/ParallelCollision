78d8933ba49631ee654e143c150dcb3618400d91
slivermasterz <lokmanslivermaster@gmail.com>
2019-04-02 22:07:29 -0700

    Revert "Better performance w/o para"
    This reverts commit 06fbbc484f333df95d75dce87419e04a486de45a.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -125,3 +125,61 @@ void ofApp::loadVbo() {
 	vbo.setVertexData(&points[0], total, GL_STATIC_DRAW);
 	vbo.setNormalData(&sizes[0], total, GL_STATIC_DRAW);
 }
+
+void ofApp::loadVboPara() {
+	if (sys->particles.size() < 1) return;
+
+	vector<ofVec3f> sizes;
+	vector<ofVec3f> points;
+
+	vector<ofVec3f> * sizesA[4];
+	vector<ofVec3f> * pointsA[4];
+
+#pragma omp parallel num_threads(4)
+	{
+		int id, i, nThreads, istart, iend;
+		id = omp_get_thread_num();
+		nThreads = omp_get_num_threads();
+		sizesA[id] = new vector<ofVec3f>;
+		pointsA[id] = new vector<ofVec3f>;
+		istart = id * sys->particles.size() / nThreads;
+		iend = (id + 1) * sys->particles.size() / nThreads;
+		if (id == nThreads - 1) iend = sys->particles.size();
+		for (i = istart; i < iend; i++) {
+			pointsA[id]->push_back(sys->particles[i].position);
+			sizesA[id]->push_back(ofVec3f(10));
+		}
+	}
+
+	divide(sizesA, 0, 3, true);
+	divide(pointsA, 0, 3, true);
+
+	// upload the data to the vbo
+	//
+	int total = (int)points.size();
+	vbo.clear();
+	vbo.setVertexData(&pointsA[0]->at(0), total, GL_STATIC_DRAW);
+	vbo.setNormalData(&sizesA[0]->at(0), total, GL_STATIC_DRAW);
+}
+
+vector<ofVec3f> * ofApp::merge(vector<ofVec3f> * A, vector<ofVec3f> * B) {
+	vector<ofVec3f> * AB = new vector<ofVec3f>; //cout << "1" << endl;
+	AB->reserve(A->size() + B->size()); //cout << "2" << endl;
+	AB->insert(AB->end(), A->begin(), A->end()); //cout << "3" << endl;
+	AB->insert(AB->end(), B->begin(), B->end()); //cout << "4" << endl;
+	return AB;
+}
+
+void ofApp::addAll(vector<ofVec3f> * from, vector<ofVec3f> * to)
+{
+	from->reserve(from->size() + to->size());
+	from->insert(from->end(), to->begin(), to->end());
+}
+
+void ofApp::divide(vector<ofVec3f> * vectors[], int left, int right, bool isLeft) {
+	if (left >= right) return;
+	int mid = left + (right - left) / 2;
+	divide(vectors, left, mid, true);
+	divide(vectors, mid + 1, right, false);
+	isLeft ? vectors[left] = merge(vectors[left], vectors[right]) : vectors[right] = merge(vectors[left], vectors[right]);
+}
