640b19cbafea0ff8fefbef308414800787f86352
slivermasterz <lokmanslivermaster@gmail.com>
2019-04-03 22:56:41 -0700

    Failed Collision result attempt

--- src/CollisionHandler.cpp
+++ src/CollisionHandler.cpp
@@ -55,7 +55,35 @@ void CollisionHandler::collisionResolve() {
 	edges.clear();
 
 	for (Pair pair : pairs) {
+		ofVec3f distanceVector = pair.b->position - pair.a->position;
+		float dist = distanceVector.distance(ofVec3f(0, 0, 0));
 
+		cout << "A: " << pair.a->position << " B: " << pair.b->position << endl;
+
+		float sine = distanceVector.y / dist;
+		float cosine = distanceVector.x / dist;
+
+
+
+		ofVec3f aTempP = ofVec3f(0, 0, 0);
+		ofVec3f bTempP = ofVec3f(cosine * distanceVector.x + sine * distanceVector.y, cosine * distanceVector.y - sine * distanceVector.x, 0);
+
+		ofVec3f aTempV = ofVec3f(cosine * pair.a->velocity.x + sine * pair.a->velocity.y, cosine * pair.a->velocity.y - sine * pair.a->velocity.x, 0);
+		ofVec3f bTempV = ofVec3f(cosine * pair.b->velocity.x + sine * pair.b->velocity.y, cosine * pair.b->velocity.y - sine * pair.b->velocity.x, 0);
+
+		ofVec3f aFinalV = ofVec3f(((pair.a->mass - pair.b->mass) * aTempV.x + 2 * pair.b->mass * bTempV.x) / (pair.a->mass + pair.b->mass), aTempV.y, 0);
+		ofVec3f bFinalV = ofVec3f(((pair.b->mass - pair.a->mass) * bTempV.x + 2 * pair.b->mass * aTempV.x) / (pair.a->mass + pair.b->mass), aTempV.y, 0);
+
+		aTempP += aFinalV;
+		bTempP += aFinalV;
+
+		aTempP = cosine * aTempP - sine * aTempP;
+		bTempP = cosine * bTempP - sine * bTempP;
+
+		pair.b->position = pair.a->position + bTempP;
+		pair.a->position += aTempP;
+		pair.a->velocity = ofVec3f(cosine * aFinalV.x - sine * aFinalV.y, cosine * aFinalV.y + sine * aFinalV.x, 0);
+		pair.b->velocity = ofVec3f(cosine * bFinalV.x - sine * bFinalV.y, cosine * bFinalV.y + sine * bFinalV.x, 0);
 	}
 	pairs.clear();
 }
