6b16e8b353e974b01d822f3aea20cacfc2147658
slivermasterz <lokmanslivermaster@gmail.com>
2019-04-18 21:24:52 -0700

    Added seed setting

--- src/ParticleGenerator.cpp
+++ src/ParticleGenerator.cpp
@@ -1,7 +1,10 @@
 #include "ParticleGenerator.h"
 
+void ParticleGenerator::setSeed(int seed) {
+	generator.seed(seed);
+}
+
 void ParticleGenerator::generateParticle(vector<Particle *> * particles) {
-	static thread_local std::mt19937 generator;
 	Particle * p = new Particle();
 	r = p->radius;
 	p->position.set(randW(), randH(), 0);
@@ -17,62 +20,6 @@ void ParticleGenerator::generateParticle(ParticleSystem * sys, int n) {
 	{
 		generateParticle(&sys->particles);
 	}
-}
-
-Particle * ParticleGenerator::generateParticlePara() {
-	static thread_local std::mt19937 generator;
-	Particle * p = new Particle();
-	p->position.set(randW(), randH(), 0);
-	std::uniform_real_distribution<float> distrubution(-1.0, 1.0);
-	ofVec3f dir = ofVec3f(distrubution(generator), distrubution(generator), 0);
-	p->velocity.set(dir * 10);
-	p->color.set(randColor());
-	return p;
-}
-
-void ParticleGenerator::generateParticlePara(ParticleSystem * sys, int n) {
-	vector<Particle*> * vectors[4];
-#pragma omp parallel num_threads(4)
-	{
-		int id, i, nThreads, istart, iend;
-		id = omp_get_thread_num();
-		nThreads = omp_get_num_threads();
-		vectors[id] = new vector<Particle*>;
-		istart = id * n / nThreads;
-		iend = (id + 1) * n / nThreads;
-		if (id == nThreads - 1) iend = n;
-		for (i = istart; i < iend; i++) {
-			generateParticle(vectors[id]);
-		}
-	}
-
-	divide(vectors, 0, 3, true);
-	addAll(&sys->particles,vectors[0]);
-	for (vector<Particle*> * vector : vectors) {
-		delete vector;
-	}
-}
-
-vector<Particle*> * ParticleGenerator::merge(vector<Particle*> * A, vector<Particle*> * B) {
-	vector<Particle*> * AB = new vector<Particle*>; //cout << "1" << endl;
-	AB->reserve(A->size() + B->size()); //cout << "2" << endl;
-	AB->insert(AB->end(), A->begin(), A->end()); //cout << "3" << endl;
-	AB->insert(AB->end(), B->begin(), B->end()); //cout << "4" << endl;
-	return AB;
-}
-
-void ParticleGenerator::addAll(vector<Particle*> * from, vector<Particle*> * to)
-{
-	from->reserve(from->size() + to->size());
-	from->insert(from->end(), to->begin(), to->end());
-}
-
-void ParticleGenerator::divide(vector<Particle*> * vectors[], int left, int right, bool isLeft) {
-	if (left >= right) return;
-	int mid = left + (right - left) / 2;
-	divide(vectors, left, mid, true);
-	divide(vectors, mid + 1, right, false);
-	isLeft ? vectors[left] = merge(vectors[left], vectors[right]) : vectors[right] = merge(vectors[left], vectors[right]);
 }
 
 int ParticleGenerator::intRand(const int & min, const int & max) {
--- src/ParticleGenerator.h
+++ src/ParticleGenerator.h
@@ -6,20 +6,17 @@
 
 class ParticleGenerator {
 	public:
+
+		void setSeed(int seed);
 		void generateParticle(vector<Particle *> *);
 		void generateParticle(ParticleSystem *,int);
 
-		void generateParticlePara(ParticleSystem *, int);
-		Particle * generateParticlePara();
-
-		vector<Particle*> * merge(vector<Particle*> * A, vector<Particle*> * B);
-		void addAll(vector<Particle*> * from, vector<Particle*> * to);
-		void divide(vector<Particle*> * vectors[], int left, int right, bool isLeft);
-	
 		int intRand(const int & min, const int & max);
 		int randW();
 		int randH();
 		ofColor randColor();
+
+		std::mt19937 generator;
 
 		int r;
 };
